a59a530c1897b07627ca49f51ace084ee3c2c29e
chen <48826388+Onethousandandtwentyfour@users.noreply.github.com>
2021-03-04 18:23:37 +0800

    Add files via upload

---
+++ unit7/翻页时钟/index.js
@@ -0,0 +1,118 @@
+import NumBlock from './num-block.js';
+var app = new Vue({
+	el: '#app',
+	components: {
+		"num-block": NumBlock,
+	},
+	data() {
+		return {
+			dateTimeSettings: {
+				year: [0, 0, 0, 0],
+				month: [0, 0],
+				date: [0, 0],
+				hours: [0, 0],
+				minutes: [0, 0],
+				seconds: [0, 0],
+			},
+			animationFrameSettings: {
+				state:'paused',
+				fps: 1000 / 1,
+				lastTime: 0,
+				iterationCount: 'infinite', //执行次数
+				hasBeenExecuted: 0, //已执行
+			},
+			beforeClickTime:0,
+			bgSkins:[
+				'imgs/clock-bg.png',
+				'imgs/clock-bg-2.png',
+				'imgs/clock-bg-3.png',
+				'imgs/clock-bg-4.png',
+			],
+			bgSkinsIndex:0,
+		}
+	},
+	computed:{
+		numKey(){
+			return (num,flag)=>{
+				return `numKey_${num}_${flag}`;
+			}
+		},
+		aniStateText(){
+			return 'running'==this.animationFrameSettings.state?'关闭':'开启';
+		},
+		skinsBgStyle(){
+			return {
+				'--clock-bg-url':`url(${this.bgSkins[this.bgSkinsIndex]})`,
+			}
+		},
+		skinsBtnsStyle(){
+			return (src,index)=>{
+				return {
+					'top':`${60 + index*30}px`,
+					'background-image':`url(${src})`,
+					'border':`2px solid ${index==this.bgSkinsIndex?'green':'transparent'}`,
+				}
+			}
+		},
+	},
+	mounted() {
+		this.animationFrame();
+	},
+	methods: {
+		changeAniState(){
+			let nowDateTime = Date.now();
+			if((nowDateTime - this.beforeClickTime)<1000){
+				return;
+			}
+			this.beforeClickTime = nowDateTime;
+			'running'==this.animationFrameSettings.state?(this.animationFrameSettings.state='paused',this.resetDateTime()):this.animationFrameSettings.state='running';
+		},
+		changeBgSkin(ev){
+			this.bgSkinsIndex = ev.currentTarget.dataset.index;
+		},
+		animationFrame() {
+			if ('strring' != (typeof this.animationFrameSettings.iterationCount) && this.animationFrameSettings.hasBeenExecuted >=
+				this.animationFrameSettings.iterationCount) {
+				return;
+			}
+			if('paused'==this.animationFrameSettings.state){
+				requestAnimationFrame(this.animationFrame);
+				return;
+			}
+			let nowTime = Date.now();
+			if ((nowTime - this.animationFrameSettings.lastTime) < (this.animationFrameSettings.fps)) {
+				requestAnimationFrame(this.animationFrame);
+				return;
+			}
+			this.animationFrameSettings.lastTime = nowTime;
+			this.updateDateTime();
+			this.animationFrameSettings.hasBeenExecuted += 1;
+			requestAnimationFrame(this.animationFrame);
+		},
+		updateDateTime() {
+			const nowDateTime = new Date(),
+				nowDay = nowDateTime.getDay();
+			this.dateTimeSettings = {
+				year: this.numFillZeroByLengthToArr(nowDateTime.getFullYear(), 4),
+				month: this.numFillZeroByLengthToArr(nowDateTime.getMonth() + 1, 2),
+				date: this.numFillZeroByLengthToArr(nowDateTime.getDate(), 2),
+				hours: this.numFillZeroByLengthToArr(nowDateTime.getHours(), 2),
+				minutes: this.numFillZeroByLengthToArr(nowDateTime.getMinutes(), 2),
+				seconds: this.numFillZeroByLengthToArr(nowDateTime.getSeconds(), 2),
+			};
+		},
+		resetDateTime(){
+			this.dateTimeSettings={
+				year: [0, 0, 0, 0],
+				month: [0, 0],
+				date: [0, 0],
+				hours: [0, 0],
+				minutes: [0, 0],
+				seconds: [0, 0],
+			};
+		},
+		numFillZeroByLengthToArr(num, length) {
+			return (num + '').padStart(length, '0').split('');
+		}
+	},
+});
---
+++ unit7/翻页时钟/num-block.js
@@ -0,0 +1,121 @@
+const numBlock = {
+	name: 'numBlock',
+	props: {
+		nextNum: {
+			type: Number,
+			default: 0,
+		}
+	},
+	data() {
+		return {
+			bgNum: 0,
+			flipNum: 0,
+			aniActive: false,
+			topNumRefName: (function() {
+				return `topNumBlock_${(Math.random()*100).toFixed(0)}`
+			})(),
+			bottomNumRefName: (function() {
+				return `bottomNumBlock_${(Math.random()*100).toFixed(0)}`
+			})(),
+			numPosition: {
+				top: {
+					x: 0,
+					y: 0,
+				},
+				bottom: {
+					x: 0,
+					y: 0,
+				}
+			},
+		}
+	},
+	computed: {
+		aniActiveClass() {
+			return this.aniActive ? 'active' : ''
+		},
+		bgPositionStyle() {
+			return (position) => {
+				return {
+					'background-position-x': `-${this.numPosition[position].x}px`,
+					'background-position-y': `-${this.numPosition[position].y}px`,
+				}
+			}
+		},
+	},
+	watch: {
+		nextNum(newVal) {
+			this.aniActive = true;
+			this.flipNum = this.nextNum;
+		}
+	},
+	mounted() {
+		// this.initWindowVisibility();
+		this.updateBgPosition();
+	},
+	methods: {
+		initWindowVisibility() {
+			//监听网页可见||不可见
+			let hiddenProperty = 'hidden' in document ? 'hidden' :
+				'webkitHidden' in document ? 'webkitHidden' :
+				'mozHidden' in document ? 'mozHidden' :
+				null;
+			let visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
+			let onVisibilityChange = () => {
+				if (!document[hiddenProperty]) {
+					this.onShow();
+				} else {
+					this.onHide();
+				}
+			}
+			document.addEventListener(visibilityChangeEvent, onVisibilityChange)
+		},
+		onShow() {
+			console.log("show");
+		},
+		onHide() {
+			console.log("hide");
+		},
+		updateBgPosition() {
+			const {
+				x: tx,
+				y: ty
+			} = this.$refs[this.topNumRefName].getBoundingClientRect(), {
+				x: bx,
+				y: by
+			} = this.$refs[this.bottomNumRefName].getBoundingClientRect();
+			this.numPosition = {
+				top: {
+					x: tx,
+					y: ty,
+				},
+				bottom: {
+					x: bx,
+					y: by,
+				}
+			};
+		},
+		numAnimationEnd(ev) {
+			if ('flip-down' == ev.animationName) {
+				this.aniActive = false;
+				this.bgNum = this.flipNum;
+			}
+		},
+	},
+	template: `<div class="num-outer-box"  :class="aniActiveClass">
+					<div class="num-box top-num" :style="bgPositionStyle('top')" :ref="topNumRefName">
+						<div class="num-inner-box top-num-text">{{flipNum}}</div>
+					</div>
+					<!-- <div class="num-box top-shadow hide-shadow-ani"></div>-->
+					<div class="num-box top-center flip-up-ani" :style="bgPositionStyle('top')">
+						<div class="num-inner-box top-num-text" >{{bgNum}}</div>
+					</div>
+					<div class="num-box bottom-num" :style="bgPositionStyle('bottom')" :ref="bottomNumRefName">
+						<div class="num-inner-box bottom-num-text">{{bgNum}}</div>
+					</div>
+					<!-- <div class="num-box bottom-shadow show-shadow-ani"></div>-->
+					<div class="num-box bottom-center flip-down-ani" @animationend="numAnimationEnd" :style="bgPositionStyle('bottom')">
+						<div class="num-inner-box bottom-num-text">{{flipNum}}</div>
+					</div>
+				</div>`,
+};
+export default numBlock;
